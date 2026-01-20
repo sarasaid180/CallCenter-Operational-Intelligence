80cb8060ea6b889cc34bcddaa179df056b5b0efa
Sara Said <sara.mohammedali180@gmail.com>
2026-01-20 03:02:41 +0200

    Update 4_load_data.sql

--- 01_data_foundation/4_load_data.sql
+++ 01_data_foundation/4_load_data.sql
@@ -10,56 +10,58 @@ Script Purpose:
 USE CallCenterDB;
 GO
 
--- =============================================================
--- 1. CLEAN EXISTING DATA (Bottom-to-Top Order)
--- =============================================================
-PRINT '>> Cleaning existing data...';
-TRUNCATE TABLE dbo.Surveys;
-TRUNCATE TABLE dbo.Calls;
-DELETE FROM dbo.Agents; -- Using DELETE because TRUNCATE is blocked by FK
-GO
+
 
 -- =============================================================
 -- 2. INSERT AGENTS DATA 
 -- =============================================================
+TRUNCATE TABLE dbo.Agents;
+
 PRINT '>> Inserting Data Into: dbo.Agents';
 BULK INSERT dbo.Agents
-FROM 'D:\Programs\pycharm\PythonProject\agents_data.csv'
-WITH (
-    FIRSTROW        = 2,
-    FIELDTERMINATOR = ',',
-    ROWTERMINATOR   = '\n',
-    TABLOCK
+FROM 'D:\My Projects\SQL Projects\call_centerDB\agents_data.csv'
+With(
+	 FIRSTROW = 2,
+	 FIELDTERMINATOR = ',',
+	 ROWTERMINATOR = '\n',
+	 TABLOCK
 );
 GO
+
+
 
 -- =============================================================
 -- 3. INSERT CALLS DATA
 -- =============================================================
+TRUNCATE TABLE dbo.Calls;
+
 PRINT '>> Inserting Data Into: dbo.Calls';
 BULK INSERT dbo.Calls
-FROM 'D:\Programs\pycharm\PythonProject\calls_data.csv'
-WITH (
-    FIRSTROW        = 2,
-    FIELDTERMINATOR = ',',
-    ROWTERMINATOR   = '\n',
-    TABLOCK
+FROM 'D:\My Projects\SQL Projects\call_centerDB\calls_data.csv'
+With(
+	 FIRSTROW = 2,
+	 FIELDTERMINATOR = ',',
+	 ROWTERMINATOR = '\n',
+	 TABLOCK
 );
 GO
 
 -- =============================================================
 -- 4. INSERT SURVEYS DATA
 -- =============================================================
+TRUNCATE TABLE dbo.Surveys;
+
 PRINT '>> Inserting Data Into: dbo.Surveys';
 BULK INSERT dbo.Surveys
-FROM 'D:\Programs\pycharm\PythonProject\surveys_data.csv'
-WITH (
-    FIRSTROW        = 2,
-    FIELDTERMINATOR = ',',
-    ROWTERMINATOR   = '\n',
-    TABLOCK
+FROM 'D:\My Projects\SQL Projects\call_centerDB\surveys_data.csv'
+With(
+	 FIRSTROW = 2,
+	 FIELDTERMINATOR = ',',
+	 ROWTERMINATOR = '\n',
+	 TABLOCK
 );
 GO
 
 PRINT '>> Data Load Complete!';
 
+--SELECT * FROM CallCenterDB.dbo.Surveys
